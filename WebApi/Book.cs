08d8a98a50e76787ca637c3bfad5917b0475f4dc
Taner Özer <tanerozer16@gmail.com>
2022-09-09 20:00:13 +0300

    book edited

---
+++ WebApi/Book.cs
@@ -0,0 +1,13 @@
+namespace WebApi{
+
+    public class Book{
+        
+        public int Id { get; set; }
+        public string title { get; set; }
+        public int genreId { get; set; }
+        public int pageCount{ get; set; }
+        public DateTime publishDate { get; set; }
+
+
+    }
+} 
