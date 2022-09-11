01e891b4f53bb0623a3cc8d31de52902e284a74c
Taner Özer <tanerozer16@gmail.com>
2022-09-11 15:58:56 +0300

    edited

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.DBOperations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// builder.Services.AddDbContext(Options => Options.UseInMemoryDatabase(databaseName:"BookStoreDB"));
+builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseInMemoryDatabase(databaseName: "BookStoreDB"));
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope()) { 
+    var services = scope.ServiceProvider;
+    DataGenerator.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,3 +32,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
