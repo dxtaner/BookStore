9f68290e13626b66683536e39052348e35f91436
Taner Özer <tanerozer16@gmail.com>
2022-09-15 19:34:00 +0300

    Genre Entities added

---
+++ WebApi/Entities/Genre.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Entities
+{
+
+    public class Genre{
+        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string name { get; set; }
+        public bool isActive { get; set; } = true;
+
+    }
+
+}
