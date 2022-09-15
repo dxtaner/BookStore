89dff5afe0a1339a282b8e18348d6cc5c715fad2
Taner Özer <tanerozer16@gmail.com>
2022-09-15 19:36:07 +0300

    DeleteGenreCommand add

---
+++ WebApi/Application/GenreOperations/Commands/DeleteGenre/DeleteGenreCommand.cs
@@ -0,0 +1,27 @@
+using WebApi.DBOperations;
+
+namespace WebApi.Aplication.GenreOperations.DeleteGenre{
+
+    public class DeleteGenreCommand
+    {
+        public int genreId { get; set; }
+        private readonly BookStoreDbContext _dbContext;
+        public DeleteGenreCommand(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Handle()
+        {
+            var genre = _dbContext.Genres.SingleOrDefault(x => x.Id == genreId);
+            if (genre ==null)
+            {
+                throw new InvalidOperationException("Kitap türü bulunamadı!");
+            }
+
+            _dbContext.Genres.Remove(genre);
+            _dbContext.SaveChanges();
+
+        }
+    }
+}
