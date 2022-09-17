bcfd657f8b4a3d4d21bdfbf1c1f6b209dcd9e6b6
Taner Özer <tanerozer16@gmail.com>
2022-09-17 12:25:53 +0300

    GetAuthorsQuery add

---
+++ WebApi/Application/AuthorOperations/Queries/GetAuthors/GetAuthorsQuery.cs
@@ -0,0 +1,35 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WebApi.DBOperations;
+using WebApi.Entities;
+
+namespace WebApi.Application.AuthorOperations.Queries.GetAuthors
+{
+    public class GetAuthorsQuery
+    {
+        private readonly BookStoreDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetAuthorsQuery(BookStoreDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public List<AuthorsViewModel> Handle()
+        {
+            var authorList = _dbContext.Authors.Include(a => a.Books).OrderBy(a => a.authorId).ToList();
+
+            List<AuthorsViewModel> vm = _mapper.Map<List<AuthorsViewModel>>(authorList);
+
+            return vm;
+        }
+        public class AuthorsViewModel
+        {
+            public string authorName { get; set; }
+            public string authorSurName { get; set; }
+            public List<Book> Books { get; set; }
+            public DateTime authorBirthdayDate { get; set; }
+        }
+    }
+}
