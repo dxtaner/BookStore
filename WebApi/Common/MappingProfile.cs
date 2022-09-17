5e47597fe2dcebd7db1fe263c3b9497498c6849d
Taner Özer <tanerozer16@gmail.com>
2022-09-17 12:26:21 +0300

    AuthorMap add

--- WebApi/Common/MappingProfile.cs
+++ WebApi/Common/MappingProfile.cs
@@ -1,6 +1,12 @@
 using AutoMapper;
 using WebApi.Entities;
-using WebApi.Applications.GenreOperations.GetGenreDetail;
+using WebApi.Application.BookOperations.Commands.CreateBook;
+using WebApi.Aplication.GenreOperations.Queries.GetGenres;
+using WebApi.Application.GenreOperations.Queries.GetGenreDetail;
+using WebApi.Application.AuthorOperations.Commands.CreateAuthor;
+using static WebApi.Application.AuthorOperations.Queries.GetAuthors.GetAuthorsQuery;
+using WebApi.Application.BookOperations.Queries.GetBookDetail;
+using static WebApi.Application.BookOperations.Queries.GetBooks.GetBooksQuery;
 
 namespace WebApi.Common
 {
@@ -10,9 +16,12 @@ namespace WebApi.Common
         {
             CreateMap<CreateBookModel, Book>();
             CreateMap<Book, GetBookDetailViewModel>().ForMember(dest => dest.genreId, opt => opt.MapFrom(src => src.genre.name));
-            CreateMap<Book, BooksViewModel>().ForMember(dest => dest.genreId, opt => opt.MapFrom(src =>src.genre.name));
+            CreateMap<Book, BooksViewModel>().ForMember(dest => dest.genreId, opt => opt.MapFrom(src => src.genre.name));
             CreateMap<Genre, GenreViewModel>();
             CreateMap<Genre, GenreDetailViewModel>();
+            CreateMap<Author, AuthorsViewModel>();
+            CreateMap<CreateAuthorModel, Author>();
+
 
         }
     }
