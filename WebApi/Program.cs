bbf000028c8c4d83d817921afc473ed5ca411fd7
Taner Özer <tanerozer16@gmail.com>
2022-09-17 12:37:00 +0300

    edited project

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -2,6 +2,8 @@ using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using WebApi.DBOperations;
 using WebApi.Middlewares;
+using WebApi.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,8 @@ builder.Services.AddSwaggerGen();
 // builder.Services.AddDbContext(Options => Options.UseInMemoryDatabase(databaseName:"BookStoreDB"));
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseInMemoryDatabase(databaseName: "BookStoreDB"));
+builder.Services.AddSingleton<ILoggerService,ConsoleLogger>();
+
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope()) { 
