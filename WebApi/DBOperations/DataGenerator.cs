d19a7e7af6eb2ec56f90d29c754b13cdff039524
Taner Özer <tanerozer16@gmail.com>
2022-09-17 12:34:32 +0300

    edited

--- WebApi/DBOperations/DataGenerator.cs
+++ WebApi/DBOperations/DataGenerator.cs
@@ -1,7 +1,5 @@
-using System;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-
+using WebApi.Entities;
 
 namespace WebApi.DBOperations
 {
@@ -18,16 +16,75 @@ namespace WebApi.DBOperations
                     return;   // Data was already seeded
                 }
 
+                var authors = new List<Author>{
+                new Author
+                {
+                    authorId = 1,
+                    authorName = "Andrew",
+                    authorSurName = "Tate",
+                    authorBirthdayDate = new DateTime(1995, 02, 12),
+                    Books = new List<Book>()
+                    {
+                    new Book { title = "Introduction to Machine Learning"},
+                    new Book { title = "Advanced Topics in Machine Learning"},
+                    new Book { title = "Introduction to Computing"}
+                    }
+                },
+                new Author
+                {
+                    authorId = 2,
+                    authorName = "William",
+                    authorSurName = "Hitch",
+                    authorBirthdayDate = new DateTime(1965, 05, 07),
+                    Books = new List<Book>()
+                    {
+                    new Book { title = "Introduction to Machine Learning"},
+                    new Book { title = "Advanced Topics in Machine Learning"},
+                    new Book { title = "Introduction to Computing"}
+                    }
+                },
+                new Author
+                {
+                    authorId = 3,
+                    authorName = "Henry",
+                    authorSurName = "Ford",
+                    authorBirthdayDate = new DateTime(1986, 07, 09),
+                    Books = new List<Book>()
+                    {
+                    new Book { title = "Introduction to Machine Learning"},
+                    new Book { title = "Advanced Topics in Machine Learning"},
+                    new Book { title = "Introduction to Computing"}
+                    }
+                }
+                };
+
+                context.Genres.AddRange(
+                  new Genre
+                  {
+                      name = "PersonalGrowth"
+                  },
+                  new Genre
+                  {
+                      name = "ScienceFiction"
+                  },
+                  new Genre
+                  {
+                      name = "Noval"
+                  }
+                );
+
                 context.Books.AddRange(
-                   new Book()
-                   {
-                    //    id=1     
-                       title = "Lean Startup",
-                       genreId = (int)1, // Personal Growth
-                       pageCount = 200,
-                       publishDate = new DateTime(2001, 06, 12)
-                   });
-                   
+                new Book()
+                {
+                    title = "Lean Startup",
+                    genreId = (int)1, // Personal Growth
+                    authorId = 1,
+                    pageCount = 200,
+                    publishDate = new DateTime(2001, 06, 12)
+                });
+
+                context.Authors.AddRange(authors);
+                
 
                 context.SaveChanges();
             }
