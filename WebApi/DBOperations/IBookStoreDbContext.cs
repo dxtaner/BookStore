bcf2693a3f42fca1a8d7c31789ba12f0bd93d9e8
Taner Özer <tanerozer16@gmail.com>
2022-09-18 15:45:52 +0300

    user add

--- WebApi/DBOperations/IBookStoreDbContext.cs
+++ WebApi/DBOperations/IBookStoreDbContext.cs
@@ -1,13 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using WebApi.Entities;
 
-namespace WebApi.DBOperations{
-    public  interface IBookStoreDbContext
+namespace WebApi.DBOperations
+{
+    public interface IBookStoreDbContext
     {
         DbSet<Book> Books { get; set; }
         DbSet<Genre> Genres { get; set; }
         DbSet<Author> Authors { get; set; }
+        DbSet<User> Users { get; set; }
+
         int SaveChanges();
-        
+
     }
 }
